0ccf2f30bafa924b3c7cc96f29167cf066fc70d8
Daniel _ <danieltmx333@gmail.com>
2024-04-14 00:37:29 +0700

    pembuatan NewsListAdapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.hobbyapp"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.hobbyapp"
         minSdk = 24
--- app/src/main/java/com/example/hobbyapp/view/NewsListAdapter.kt
+++ app/src/main/java/com/example/hobbyapp/view/NewsListAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.hobbyapp.view
 
-class NewsListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hobbyapp.databinding.NewsListItemBinding
+import com.example.hobbyapp.model.News
+
+
+class NewsListAdapter(val newsList:ArrayList<News>): RecyclerView.Adapter<NewsListAdapter.NewsViewHolder>()  {
+
+    class NewsViewHolder(var binding: NewsListItemBinding) :RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val binding= NewsListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return NewsViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
